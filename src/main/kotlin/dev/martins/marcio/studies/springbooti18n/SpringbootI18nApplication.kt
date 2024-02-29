1e70227bde6069be6b4787ff0965966d0ae4cb7e
Marcio Martins <marciovmartins@hotmail.com>
2024-02-29 18:02:57 +0100

    Adding initial code downloaded from start.spring.io

---
+++ src/main/kotlin/dev/martins/marcio/studies/springbooti18n/SpringbootI18nApplication.kt
@@ -0,0 +1,11 @@
+package dev.martins.marcio.studies.springbooti18n
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootI18nApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootI18nApplication>(*args)
+}
---
+++ src/test/kotlin/dev/martins/marcio/studies/springbooti18n/SpringbootI18nApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.martins.marcio.studies.springbooti18n
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootI18nApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
